12c37c76a630b6728382620b5929199df03b98c9
CoochiecooBerrauslu <113683902+CoochiecooBerrauslu@users.noreply.github.com>
2023-03-25 18:53:21 +0300

    Update Main.java
    Explanation added.

--- ArmstrongNumbers2/src/Main.java
+++ ArmstrongNumbers2/src/Main.java
@@ -6,13 +6,15 @@ public class Main {
         for(int number = 100; number <= 999; number++)
         {
             temp = number;
-            digit3=temp%10;//3
-            temp=temp/10;//12
+            digit3=temp%10;// units digit
+            temp=temp/10;// taking out the unit digit
 
-            digit2=temp%10;//2
-            temp=temp/10;//1
+            digit2=temp%10;// tens digit
+            temp=temp/10;// taking out the tens digit
 
-            digit1=temp%10;//1
+            digit1=temp%10;// and hundreds digit
+            //Math.pow() is used for calculating the power of digit e.g. => digit1 * digit1 * digit1
+            // first part the one as a sub second one as a sup(power)
             result=((int)Math.pow(digit1,3))+((int)Math.pow(digit2,3))+((int)Math.pow(digit3,3));
 
             if(number==result){
